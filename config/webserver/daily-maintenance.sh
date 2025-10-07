5765bb6cd7432fe72643859291266bbf915d6e5c
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2025-10-06 17:47:10 -0700

    additional daily maintenance task

--- config/webserver/daily-maintenance.sh
+++ config/webserver/daily-maintenance.sh
@@ -2,4 +2,5 @@
 /usr/bin/sqlite3 /home/ec2-user/classicmacdemos/website/db.sqlite3 "UPDATE demos_play SET plays=plays-1 WHERE plays > 1;"
 /usr/bin/sqlite3 /home/ec2-user/classicmacdemos/website/db.sqlite3 "UPDATE demos_download SET downloads=downloads-1 WHERE downloads > 1;"
 /usr/bin/sqlite3 /home/ec2-user/classicmacdemos/website/db.sqlite3 "UPDATE demos_play SET plays = 0 WHERE game_id IN (select id from demos_game where game like 'Living Books%');"
+/usr/bin/sqlite3 /home/ec2-user/classicmacdemos/website/db.sqlite3 "UPDATE demos_download SET downloads = 0 WHERE game_id IN (select id from demos_game where game like 'Living Books%');"
 /usr/bin/date >> /home/ec2-user/daily-maintenance.log
