6d00f8472f6fcb6e2b14e7e5f5bb6c6f2eb93dfa
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2026-04-02 16:03:03 -0700

    add html output & no-download options to macintosh garden inventory script

--- survey/contribute-to-macintosh-garden.py
+++ survey/contribute-to-macintosh-garden.py
@@ -4,46 +4,68 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-query = """select dg.slug, dw.macintosh_garden, dg.filename, dg.filename_68k, dg.filename_3 
-    from demos_game dg join demos_website dw on dw.game_id = dg.id;
+HTML     = True # HTML output
+DOWNLOAD = False # Download demos locally
+
+query = """select dg.slug, dw.macintosh_garden, dg.filename, dg.filename_68k, dg.filename_3, dg.game
+    from demos_game dg join demos_website dw on dw.game_id = dg.id 
+    ORDER BY dg.slug;
     """
 
 subdir = "demofiles"
 
-with open(".gitignore", 'w') as f: 
-    f.write(subdir)
+if HTML:
+    print("<table><tr><th>Game</th><th>Macintosh Garden page</th><th>Demo file(s)</th></tr>")
+else:
+    with open(".gitignore", 'w') as f: 
+        f.write(subdir)
 
 con = sqlite3.connect("../website/db.sqlite3")
 cur = con.cursor()
 res = cur.execute(query)
 for result in res.fetchall():
-    print( result )
-    directory = subdir + "/" + result[0]
-    try: 
-        os.makedirs(directory, exist_ok=True)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    
-    if result[1]:
-        webloc = """<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-    <dict>
-        <key>URL</key>
-        <string>{url}</string>
-    </dict>
-    </plist>
-    """.format(url=result[1])
+    if HTML:
+        print("<tr>")
+        print("<td>{name}</td>".format(name=result[5]))
+        print("<td><a href=\"{mg}\">Macintosh Garden</a></td>".format(mg=result[1]))
+        print("<td>")
+        for i in range(2,4):
+            if result[i]:
+                print("<a href=\"{url}\">{filename}</a>".format(
+                    url="https://download.classicmacdemos.com/" + urllib.parse.quote(result[i]),
+                    filename=result[i]
+                ))
+        print("</td>")
+    else:
+        directory = subdir + "/" + result[0]
+        try: 
+            os.makedirs(directory, exist_ok=True)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        
+        if result[1]:
+            webloc = """<?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+        <plist version="1.0">
+        <dict>
+            <key>URL</key>
+            <string>{url}</string>
+        </dict>
+        </plist>
+        """.format(url=result[1])
 
-        with open(directory + "/Macintosh Garden.webloc", 'w') as f: 
-            f.write(webloc)
+            with open(directory + "/Macintosh Garden.webloc", 'w') as f: 
+                f.write(webloc)
 
-    if result[2] and not os.path.isfile(directory + "/" + result[2]):
-        subprocess.run(['curl', '-o', directory + "/" + result[2], 
-                        "https://download.classicmacdemos.com/" + urllib.parse.quote(result[2])])
-    if result[3] and not os.path.isfile(directory + "/" + result[3]):
-        subprocess.run(['curl', '-o', directory + "/" + result[3], 
-                        "https://download.classicmacdemos.com/" + urllib.parse.quote(result[3])])
-    if result[4] and not os.path.isfile(directory + "/" + result[4]):
-        subprocess.run(['curl', '-o', directory + "/" + result[4], 
-                        "https://download.classicmacdemos.com/" + urllib.parse.quote(result[4])])
+        if DOWNLOAD and result[2] and not os.path.isfile(directory + "/" + result[2]):
+            subprocess.run(['curl', '-o', directory + "/" + result[2], 
+                            "https://download.classicmacdemos.com/" + urllib.parse.quote(result[2])])
+        if DOWNLOAD and result[3] and not os.path.isfile(directory + "/" + result[3]):
+            subprocess.run(['curl', '-o', directory + "/" + result[3], 
+                            "https://download.classicmacdemos.com/" + urllib.parse.quote(result[3])])
+        if DOWNLOAD and result[4] and not os.path.isfile(directory + "/" + result[4]):
+            subprocess.run(['curl', '-o', directory + "/" + result[4], 
+                            "https://download.classicmacdemos.com/" + urllib.parse.quote(result[4])])
+
+if HTML:
+    print("</table>")
