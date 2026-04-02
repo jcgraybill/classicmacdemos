0d9632b8780ebac98db59a0a1aa1463bf7a25b7c
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2026-04-01 19:24:02 -0700

    add script to look for new or missed discs on internet archive

--- survey/survey-internet-archive.py
+++ survey/survey-internet-archive.py
@@ -1,16 +1,75 @@
-from internetarchive import get_item, search_items, get_files
-
 # https://archive.org/developers/internetarchive/python-lib.html
 
-item = get_item("macworld-cd-wwdc-2005")
-print(item.metadata)
+from internetarchive import get_item, search_items, get_files
+import os, sqlite3, sys
 
-search = search_items('collection:macworld-cds')
-for result in search:
-    print(result['identifier'])
-    fnames = [f.name for f in get_files(result['identifier'], glob_pattern='*iso')]
-    print(fnames)
+searches = [
+    'collection:macworld-cds',
+    'subject:"Macworld"'
+]
 
-#    item = get_item(result['identifier'])
-#    print(item.metadata)
+globs = [
+    '*.iso',
+    '*.toast',
+    '*.bin',
+    '*.cdr',
+    '*.sit'
+]
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+con = sqlite3.connect("../website/db.sqlite3")
+cur = con.cursor()
+query = """
+select count() from demos_source where infinite_mac_url = :url
+    OR disc2_infinite_mac_url = :url
+    OR disc3_infinite_mac_url = :url;
+"""
+
+survey_con = sqlite3.connect("survey.sqlite3")
+survey_cur = survey_con.cursor()
+survey_peek = "select status, note from internet_archive where url = :url;"
+survey_insert = """
+insert into internet_archive(url, status, note) 
+values(:url, :status, :note)
+on conflict (url) do update set status=:status, note=:note;
+"""
+
+memo = dict()
+
+print()
+for search in searches:
+    for result in search_items(search):
+        if memo.get(result['identifier']): next
+        print("https://archive.org/details/" + result['identifier'])
+        print("-" * 30)
+        for g in globs:
+            for f in get_files(result['identifier'], glob_pattern=g):
+                res = cur.execute(query, { "url": f.url })
+                if res.fetchone()[0] > 0:
+                    print("✅ {url}".format(url=f.url))
+                else:
+                    sur = survey_cur.execute(survey_peek, {"url": f.url})
+                    sr = sur.fetchone()
+                    if sr and sr[0] == 's':
+                        print("⛔️ {url}".format(url=f.url))
+                    else:
+                        print("❓ {url}".format(url=f.url))
+                        if sr and sr[1]:
+                            print(sr[1])
+                        choice = ''
+                        while(choice == ''):
+                            action = input("[s]kip forever, deal with [L]ater, be [d]one: ")
+                            if action == '': action = 'l'
+                            choice = action[0].lower()
+                            if choice == 'd': sys.exit()
+                            if choice != 's' and choice != 'l':
+                                choice = ''
+                        note = input("note: ")
+                        if sr and sr[1] and note == '': note = sr[1]
+                        survey_cur.execute(survey_insert,{ "url": f.url, "status": choice, "note": note} )
+                        survey_con.commit()
+        memo[result['identifier']] = True
+        print()
