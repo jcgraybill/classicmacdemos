345d519fd6832acae4125fe9200175a32fe560ef
jcgraybill <65628360+jcgraybill@users.noreply.github.com>
2026-06-17 13:20:46 -0700

    simplify bootstrap of analytics host

--- config/analytics/bootstrap.sh
+++ config/analytics/bootstrap.sh
@@ -1,32 +1,106 @@
 #!/bin/sh
 
-sudo dnf install git
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y qemu-guest-agent avahi-daemon
 
-echo "GitHub Deployment Key:"
-github_deployment_key=$(</dev/stdin)
-echo "$github_deployment_key" | tee /home/ec2-user/.ssh/id_ed25519
-sudo chmod 0600 /home/ec2-user/.ssh/id_ed25519
-eval "$(ssh-agent -s)"
-ssh-add /home/ec2-user/.ssh/id_ed25519
+cd /home/$USER
 
-( cd /home/ec2-user
-  curl https://artifacts.opensearch.org/releases/core/opensearch/2.12.0/opensearch-min-2.12.0-linux-arm64.tar.gz | tar xz
-  curl https://artifacts.opensearch.org/releases/core/opensearch-dashboards/2.12.0/opensearch-dashboards-min-2.12.0-linux-arm64.tar.gz | tar xz
-  git clone git@github.com:jcgraybill/classicmacdemos.git
-)
+echo -n "public ssh key: "
+read key
+mkdir -p .ssh
+echo $key | tee -a .ssh/authorized_keys
 
-cp /home/ec2-user/classicmacdemos/config/analytics/opensearch.yml /home/ec2-user/opensearch-2.12.0/config/
-cp /home/ec2-user/classicmacdemos/config/analytics/opensearch_dashboards.yml /home/ec2-user/opensearch-dashboards-2.12.0-linux-arm64/config/
+tee sync-logs.sh << EOF
+#!/bin/sh
 
-sudo cp /home/ec2-user/classicmacdemos/config/analytics/90-vm-max-map-count.conf /etc/sysctl.d/
-sudo sysctl -p
+remote_host=""
+private_key=""
 
-sudo cp /home/ec2-user/classicmacdemos/config/analytics/systemd/opensearch.service /etc/systemd/system/
-sudo cp /home/ec2-user/classicmacdemos/config/analytics/systemd/opensearch-dashboards.service /etc/systemd/system/
+if ! test -z \$remote_host && ! test -z \$private_key; then
+rsync -avP -e "ssh -i \$private_key" \
+	$USER@\$remote_host:/var/log/nginx/ \
+	/home/$USER/nginx-logs
+fi
+EOF
+chmod 755 sync-logs.sh
+
+echo "* * * * * /home/$USER/sync-logs.sh" | crontab
+
+curl https://artifacts.opensearch.org/releases/core/opensearch/2.19.5/opensearch-min-2.19.5-linux-x64.tar.gz | tar xz
+curl https://artifacts.opensearch.org/releases/core/opensearch-dashboards/2.19.5/opensearch-dashboards-min-2.19.5-linux-x64.tar.gz | tar xz
+curl https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.12.1-linux-x86_64.tar.gz | tar xz
+
+tee opensearch-2.19.5/config/opensearch.yml << EOF
+compatibility.override_main_response_version: true
+network.host: 0.0.0.0
+discovery.type: single-node
+EOF
+
+tee opensearch-dashboards-2.19.5-linux-x64/config/opensearch_dashboards.yml << EOF
+opensearchDashboards.survey.url: "false"
+server.host: "0.0.0.0"
+EOF
+
+mkdir -p nginx-logs
+./filebeat-7.12.1-linux-x86_64/filebeat -c filebeat-7.12.1-linux-x86_64/filebeat.yml modules enable nginx
+
+tee filebeat-7.12.1-linux-x86_64/modules.d/nginx.yml << EOF
+- module: nginx
+  access:
+    enabled: true
+    var.paths: ["/home/$USER/nginx-logs/access.log*"]
+  error:
+    enabled: true
+    var.paths: ["/home/$USER/nginx-logs/error.log*"]
+  ingress_controller:
+    enabled: false
+EOF
+
+sudo tee -a /etc/sysctl.d/90-vm-max-map-count.conf << EOF
+vm.max_map_count=262144
+EOF
+
+sudo tee /etc/systemd/system/opensearch.service << EOF
+[Service]
+Type=forking
+Restart=always
+User=$USER
+ExecStart=/home/$USER/opensearch-2.19.5/bin/opensearch -d -p /var/tmp/opensearch.pid
+PIDFile=/var/tmp/opensearch.pid
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee /etc/systemd/system/opensearch-dashboards.service << EOF
+[Service]
+Type=simple
+Restart=always
+User=$USER
+ExecStart=/home/$USER/opensearch-dashboards-2.19.5-linux-x64/bin/opensearch-dashboards
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo tee /etc/systemd/system/filebeat.service << EOF
+[Service]
+Type=simple
+Restart=always
+User=$USER
+ExecStart=/home/$USER/filebeat-7.12.1-linux-x86_64/filebeat -c /home/$USER/filebeat-7.12.1-linux-x86_64/filebeat.yml run
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 sudo systemctl daemon-reload
 
 sudo systemctl enable opensearch
 sudo systemctl enable opensearch-dashboards
+sudo systemctl enable filebeat
 
 sudo service opensearch start
 sudo service opensearch-dashboards start
+sudo service filebeat start
--- config/webserver/bootstrap.sh
+++ config/webserver/bootstrap.sh
@@ -13,11 +13,6 @@ echo "$github_deployment_key" | tee /home/ec2-user/.ssh/id_ed25519
 eval "$(ssh-agent -s)"
 ssh-add /home/ec2-user/.ssh/id_ed25519
 
-( cd /home/ec2-user
-  curl https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.12.1-linux-arm64.tar.gz | tar xz
-  git clone git@github.com:jcgraybill/classicmacdemos.git
-)
-
 echo "Django secret key:"
 read django_secret_key
 echo "$django_secret_key" | tee /home/ec2-user/classicmacdemos/website/secret-key.txt
@@ -30,21 +25,17 @@ sudo service nginx stop
 sudo cp /home/ec2-user/classicmacdemos/config/webserver/nginx.conf /etc/nginx/
 sudo cp /home/ec2-user/classicmacdemos/config/webserver/nginx /etc/logrotate.d/
 
-/home/ec2-user/filebeat-7.12.1-linux-arm64/filebeat -c /home/ec2-user/classicmacdemos/config/webserver/filebeat.yml modules enable nginx
 
 sudo cp /home/ec2-user/classicmacdemos/config/webserver/systemd/backupdb.service /etc/systemd/system/
 sudo cp /home/ec2-user/classicmacdemos/config/webserver/systemd/backupdb.timer /etc/systemd/system/
-sudo cp /home/ec2-user/classicmacdemos/config/webserver/systemd/filebeat.service /etc/systemd/system/
 sudo cp /home/ec2-user/classicmacdemos/config/webserver/systemd/uwsgi.service /etc/systemd/system/
 sudo systemctl daemon-reload
 
 sudo service backupdb enable
-sudo service filebeat enable
 sudo service uwsgi enable
 sudo service nginx enable
 
 sudo service backupdb start
-sudo service filebeat start
 sudo service uwsgi start
 sudo service nginx start
 
@@ -52,5 +43,4 @@ sudo cp /home/ec2-user/classicmacdemos/config/webserver/webroot/* /usr/share/ngi
 sudo mkdir -p /usr/share/nginx/html/.well-known/acme-challenge
 sudo chown ec2-user:ec2-user /usr/share/nginx/html/.well-known/acme-challenge
 
-echo "Remember to set output.elasticsearch.hosts in /home/ec2-user/classicmacdemos/config/webserver/filebeat.yml"
 echo "Remember to add /home/ec2-user/classicmacdemos/website/db.sqlite3"
